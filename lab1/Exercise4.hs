012fa7263fb215fa16dc7e07807d97ef3296a822
Atroa-sh <kacper.piastowicz@onet.eu>
2023-09-20 09:46:34 +0200

    ex4

--- lab1/Exercise4.hs
+++ lab1/Exercise4.hs
@@ -1,10 +1,11 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use null" #-}
-module Lab1_Kacper where
+module Exercise2 where
 
 import Data.List
-import System.Random
-import Test.QuickCheck
+import Data.List (permutations)
+import Data.ByteString (filter)
+import Control.Exception (assert)
 -- import Lecture1
 
 
@@ -12,6 +13,7 @@ isDerangement :: (Eq a, Ord a) => [a] -> [a] -> Bool
 isDerangement x y
     | length x /= length y = False
     | sort x /= sort y = False
+    | x == [] = False
     | otherwise = _isDerangement x y
 
 
@@ -22,3 +24,26 @@ _isDerangement (x:xs) (y:ys)
   | otherwise = _isDerangement xs ys
 
 
+deran:: Int -> [[Int]]
+deran n = Data.List.filter (isDerangement [0..n-1]) (Data.List.permutations [0..n-1])
+
+-- we need to test potentially unwanted inputs, different lengths, empty ones, 
+main :: IO ()
+main = do
+  putStrLn (if isDerangement [0,1,2] [1,0,2] then "Passed1" else "Failed1")
+  putStrLn (if isDerangement [0,1,2,3] [1,0,2] then "Passed2" else "Failed2")
+  putStrLn (if isDerangement ([]::[Int]) ([]::[Int]) then "Passed3" else "Failed3")
+  putStrLn (if isDerangement [0,1,2] [3,4,5] then "Passed4" else "Failed4")
+  putStrLn (if isDerangement [0,1,2,3] [1,0,3,2] then "Passed5" else "Failed5")
+
+
+--order of properties:
+-- both inputs need to be lists
+-- both lists need to be equal in length
+-- both lists need to consists of the same elements
+-- no element in those lists can occupy same index
+
+--can we test it automatically?
+-- problem is that if we were to automate it we would need a function that would generate derengements and we 
+-- would be certain of its corectness. How would we be certain of that? Maybe buy checking if all of its outputs are derangements?
+-- that would then require that would correctly check if lists are derangements and cycle repeats
