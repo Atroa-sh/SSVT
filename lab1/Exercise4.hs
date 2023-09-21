109edddf26b8c23fd253d2840cd1f13fbc8c45f1
Atroa-sh <kacper.piastowicz@onet.eu>
2023-09-21 12:25:37 +0200

    update

--- lab1/Exercise4.hs
+++ lab1/Exercise4.hs
@@ -5,7 +5,7 @@ module Exercise2 where
 import Data.List
 import Data.List (permutations)
 import Data.ByteString (filter)
-import Control.Exception (assert)
+import Test.QuickCheck
 -- import Lecture1
 
 
@@ -15,6 +15,8 @@ isDerangement x y
     | sort x /= sort y = False
     | x == [] = False
     | otherwise = _isDerangement x y
+    -- do we need to check if numbers are in 0..n-1 range
+    -- are lists with duplicates a valid input
 
 
 -- we can check a bunch of properties before doing most of the work and eliminate a lot of incorrect inputs
@@ -29,23 +31,53 @@ _isDerangement (x:xs) (y:ys)
 deran:: Int -> [[Int]]
 deran n = Data.List.filter (isDerangement [0..n-1]) (Data.List.permutations [0..n-1])
 
--- we need to test potentially unwanted inputs, different lengths, empty ones, 
+
+prop_reverseInput :: Ord a => [a] -> [a] -> Bool
+prop_reverseInput a b = isDerangement a b == isDerangement b a
+
+prop_transitive :: Ord a => [a] -> [a] -> [a] -> Bool
+prop_transitive a b c = if (a /= c && isDerangement a b == True && isDerangement b c == True) then isDerangement a c == True else True
+
+prop_unequal :: Ord a => [a] -> [a] -> Bool
+prop_unequal a b = if (length a) /= (length b) then isDerangement a b == False else True 
+
+prop_sameElements :: Ord a => [a] -> [a] -> Bool
+prop_sameElements a b = if (sort a) /= (sort b) then isDerangement a b == False else True
+
 main :: IO ()
 main = do
-  putStrLn (if isDerangement [0,1,2] [1,0,2] then "Passed1" else "Failed1")
-  putStrLn (if isDerangement [0,1,2,3] [1,0,2] then "Passed2" else "Failed2")
-  putStrLn (if isDerangement ([]::[Int]) ([]::[Int]) then "Passed3" else "Failed3")
-  putStrLn (if isDerangement [0,1,2] [3,4,5] then "Passed4" else "Failed4")
-  putStrLn (if isDerangement [0,1,2,3] [1,0,3,2] then "Passed5" else "Failed5")
+  quickCheck (prop_reverseInput :: [Int] -> [Int] -> Bool)
+  quickCheck (prop_sameElements :: [Int] -> [Int] -> Bool)
+  quickCheck (prop_unequal :: [Int] -> [Int] -> Bool)
+  quickCheck (prop_transitive :: [Int] -> [Int] -> [Int] -> Bool)
+
+  quickCheck $ forAll [[1,2,3],[2,3,1], [], [1,2,3,4]] prop_reverseInput
+  -- putStrLn (if isDerangement [0,1,2] [1,0,2] then "Passed1" else "Failed1")
+  -- putStrLn (if isDerangement [0,1,2,3] [1,0,2] then "Passed2" else "Failed2")
+  -- putStrLn (if isDerangement ([]::[Int]) ([]::[Int]) then "Passed3" else "Failed3")
+  -- putStrLn (if isDerangement [0,1,2] [3,4,5] then "Passed4" else "Failed4")
+  -- putStrLn (if isDerangement [0,1,2,3] [1,0,3,2] then "Passed5" else "Failed5")
 
 
---order of properties:
--- both inputs need to be lists
--- both lists need to be equal in length
--- both lists need to consists of the same elements
--- no element in those lists can occupy same index
 
---can we test it automatically?
--- problem is that if we were to automate it we would need a function that would generate derengements and we 
--- would be certain of its corectness. How would we be certain of that? Maybe buy checking if all of its outputs are derangements?
--- that would then require that would correctly check if lists are derangements and cycle repeats
+{-
+order of properties:
+both inputs need to be lists
+reflective relation
+transitive relation
+both lists need to be equal in length  
+both lists need to consists of the same elements
+no element in those lists can occupy same index
+-}
+
+{-
+can we test it automatically?
+problem is that if we were to automate it we would need a function that would generate derengements and we 
+would be certain of its corectness. How would we be certain of that? Maybe buy checking if all of its outputs are derangements?
+that would then require that would correctly check if lists are derangements and cycle repeats
+
+another issue is that if we were to tests it would be nice to do it for all possible inputs. 
+there is a countable infinity of them so this is not feasale
+
+indication of time spent: 5h
+-}
---
+++ lab1/Exercise6.hs
@@ -0,0 +1,215 @@
+module Exercise6 where
+
+import Data.List
+import Data.Char
+import Test.QuickCheck
+import Data.Char (GeneralCategory(Format))
+
+
+update :: Eq a => (a -> b) -> (a,b) -> a -> b
+update f (x,y) = \ z -> if x == z then y else f z 
+
+updates :: Eq a => (a -> b) -> [(a,b)] -> a -> b
+updates = foldl update
+
+infix 1 --> 
+(-->) :: Bool -> Bool -> Bool
+p --> q = (not p) || q
+
+type Name = Int
+
+data Form = Prop Name
+          | Neg  Form
+          | Cnj [Form]
+          | Dsj [Form]
+          | Impl Form Form 
+          | Equiv Form Form 
+          deriving (Eq,Ord)
+
+instance Show Form where 
+  show (Prop x)   = show x
+  show (Neg f)    = '-' : show f 
+  show (Cnj fs)     = "*(" ++ showLst fs ++ ")"
+  show (Dsj fs)     = "+(" ++ showLst fs ++ ")"
+  show (Impl f1 f2)  = "(" ++ show f1 ++ "==>" 
+                           ++ show f2 ++ ")"
+  show (Equiv f1 f2)  = "(" ++ show f1 ++ "<=>" 
+                           ++ show f2 ++ ")"
+
+showLst,showRest :: [Form] -> String
+showLst [] = ""
+showLst (f:fs) = show f ++ showRest fs
+showRest [] = ""
+showRest (f:fs) = ' ': show f ++ showRest fs
+
+p = Prop 1
+q = Prop 2
+r = Prop 3 
+
+form1 = Equiv (Impl p q) (Impl (Neg q) (Neg p))
+form2 = Equiv (Impl p q) (Impl (Neg p) (Neg q))
+form3 = Impl (Cnj [Impl p q, Impl q r]) (Impl p r)
+
+propNames :: Form -> [Name]
+propNames = sort.nub.pnames where 
+  pnames (Prop name) = [name]
+  pnames (Neg f)  = pnames f
+  pnames (Cnj fs) = concatMap pnames fs
+  pnames (Dsj fs) = concatMap pnames fs
+  pnames (Impl f1 f2)  = concatMap pnames [f1,f2]
+  pnames (Equiv f1 f2) = concatMap pnames [f1,f2]
+
+type Valuation = [(Name,Bool)]
+
+-- | all possible valuations for lists of prop letters
+genVals :: [Name] -> [Valuation]
+genVals [] = [[]]
+genVals (name:names) = 
+  map ((name,True) :) (genVals names)
+  ++ map ((name,False):) (genVals names)
+
+-- | generate all possible valuations for a formula
+allVals :: Form -> [Valuation]
+allVals = genVals . propNames
+
+type ValFct = Name -> Bool
+
+val2fct :: Valuation -> ValFct
+val2fct = updates (\ _ -> undefined)
+
+fct2val :: [Name] -> ValFct -> Valuation
+fct2val domain f = map (\x -> (x,f x)) domain 
+
+evl :: Valuation -> Form -> Bool
+evl [] (Prop c)    = error ("no info: " ++ show c)
+evl ((i,b):xs) (Prop c)
+     | c == i    = b
+     | otherwise = evl xs (Prop c)
+evl xs (Neg f)  = not (evl xs f)
+evl xs (Cnj fs) = all (evl xs) fs
+evl xs (Dsj fs) = any (evl xs) fs
+evl xs (Impl f1 f2) = evl xs f1 --> evl xs f2
+evl xs (Equiv f1 f2) = evl xs f1 == evl xs f2
+
+satisfiable :: Form -> Bool
+satisfiable f = any (\ v -> evl v f) (allVals f)
+
+data Token 
+      = TokenNeg
+      | TokenCnj
+      | TokenDsj
+      | TokenImpl
+      | TokenEquiv 
+      | TokenInt Int 
+      | TokenOP
+      | TokenCP
+ deriving (Show,Eq)
+
+lexer :: String -> [Token]
+lexer [] = []
+lexer (c:cs) | isSpace c = lexer cs
+             | isDigit c = lexNum (c:cs) 
+lexer ('(':cs) = TokenOP : lexer cs
+lexer (')':cs) = TokenCP : lexer cs
+lexer ('*':cs) = TokenCnj : lexer cs
+lexer ('+':cs) = TokenDsj : lexer cs
+lexer ('-':cs) = TokenNeg : lexer cs 
+lexer ('=':'=':'>':cs) = TokenImpl : lexer cs
+lexer ('<':'=':'>':cs) = TokenEquiv : lexer cs
+lexer (x:_) = error ("unknown token: " ++ [x])
+
+lexNum cs = TokenInt (read num) : lexer rest
+     where (num,rest) = span isDigit cs
+
+type Parser a b = [a] -> [(b,[a])]
+
+succeed :: b -> Parser a b
+succeed x xs = [(x,xs)]
+
+parseForm :: Parser Token Form 
+parseForm (TokenInt x: tokens) = [(Prop x,tokens)]
+parseForm (TokenNeg : tokens) =
+  [ (Neg f, rest) | (f,rest) <- parseForm tokens ]
+parseForm (TokenCnj : TokenOP : tokens) = 
+  [ (Cnj fs, rest) | (fs,rest) <- parseForms tokens ]
+parseForm (TokenDsj : TokenOP : tokens) = 
+  [ (Dsj fs, rest) | (fs,rest) <- parseForms tokens ]
+parseForm (TokenOP : tokens) = 
+  [ (Impl f1 f2, rest) | (f1,ys) <- parseForm tokens,
+                         (f2,rest) <- parseImpl ys ]
+   ++
+  [ (Equiv f1 f2, rest) | (f1,ys) <- parseForm tokens,
+                          (f2,rest) <- parseEquiv ys ]
+parseForm tokens = []
+
+parseForms :: Parser Token [Form] 
+parseForms (TokenCP : tokens) = succeed [] tokens
+parseForms tokens = 
+   [(f:fs, rest) | (f,ys) <- parseForm tokens, 
+                   (fs,rest) <- parseForms ys ]
+
+parseImpl :: Parser Token Form
+parseImpl (TokenImpl : tokens) = 
+  [ (f,ys) | (f,y:ys) <- parseForm tokens, y == TokenCP ]
+parseImpl tokens = []
+
+parseEquiv :: Parser Token Form
+parseEquiv (TokenEquiv : tokens) = 
+  [ (f,ys) | (f,y:ys) <- parseForm tokens, y == TokenCP ]
+parseEquiv tokens = []
+
+parse :: String -> [Form]
+parse s = [ f | (f,_) <- parseForm (lexer s) ]
+
+arrowfree :: Form -> Form 
+arrowfree (Prop x) = Prop x 
+arrowfree (Neg f) = Neg (arrowfree f)
+arrowfree (Cnj fs) = Cnj (map arrowfree fs)
+arrowfree (Dsj fs) = Dsj (map arrowfree fs)
+arrowfree (Impl f1 f2) = 
+  Dsj [Neg (arrowfree f1), arrowfree f2]
+arrowfree (Equiv f1 f2) = 
+  Dsj [Cnj [f1', f2'], Cnj [Neg f1', Neg f2']]
+  where f1' = arrowfree f1
+        f2' = arrowfree f2
+
+nnf :: Form -> Form 
+nnf (Prop x) = Prop x
+nnf (Neg (Prop x)) = Neg (Prop x)
+nnf (Neg (Neg f)) = nnf f
+nnf (Cnj fs) = Cnj (map nnf fs)
+nnf (Dsj fs) = Dsj (map nnf fs)
+nnf (Neg (Cnj fs)) = Dsj (map (nnf.Neg) fs)
+nnf (Neg (Dsj fs)) = Cnj (map (nnf.Neg) fs)
+nnf (Impl f g) = nnf (Dsj [(Neg f),g])
+nnf (Neg (Impl f g)) = nnf (Neg (Dsj [Neg f, g]))
+nnf (Equiv f g) = let a = (Cnj [f,g])
+                      b = (Cnj [(Neg f), (Neg g)])
+                      in nnf (Dsj [a,b])
+nnf (Neg (Equiv f g)) = let a = (Dsj [f,g])
+                            b = (Dsj [(Neg f), (Neg g)])
+                            in nnf (Cnj [a,b])
+{-
+I've implemented additional cases to deal with Implications and Equivalences.
+Algorithm follows regular laws of conversion.
+-}
+
+cnf :: Form -> Form
+cnf f = cnf' (nnf f)
+  where
+    cnf' :: Form -> Form
+    cnf' (Cnj fs) = Cnj (map cnf' fs)
+    cnf' (Dsj (x:xs)) = foldr dist (cnf' x) xs
+    cnf' (Prop x) = Prop x
+    cnf' (Neg (Prop x)) = Neg (Prop x)
+
+    dist :: Form -> Form -> Form
+    dist (Cnj fs) f = Cnj (map (dist f) fs)
+    dist f (Cnj fs) = Cnj (map (dist f) fs)
+    dist f g = Dsj [f, g]
+
+{-
+cnf is also a nnf form. We can call nnf first to reduce cnf to only necessary functions.
+dist function stands for distribution law. Foldr allows us to accumulate result of distribution in cases when we have a
+disjunction of more than 2 forms.
+-}
