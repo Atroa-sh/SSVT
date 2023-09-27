a47062d9420dfbb4cf9e6fe630d8800e6e74edc2
Devrim Yugnuk <devrim.yugnuk@student.uva.nl>
2023-09-27 12:53:29 +0200

    1

--- lab2/Exercise1.hs
+++ lab2/Exercise1.hs
@@ -4,3 +4,16 @@ import Data.List
 import LTS
 import Test.QuickCheck
 
+{-
+List of factors that result in invalid IOLTS's:
+    - Duplicate states
+    - Duplicate labels
+    - Duplicate transitions
+    - Transitions to non-existing states
+    - Transitions from non-existing states
+    - Transitions with non-existing labels
+    - Transitions with non-existing states
+-}
+
+-- Checks if the given IOLTS is valid
+validateLTS :: IOLTS -> Bool
