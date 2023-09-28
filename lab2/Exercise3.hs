c7f0d7a29f4fac2908aacade59673c62eafa0cc3
Devrim Yugnuk <devrim.yugnuk@student.uva.nl>
2023-09-28 12:20:22 +0200

    Exercise3.hs

--- lab2/Exercise3.hs
+++ lab2/Exercise3.hs
@@ -8,4 +8,16 @@ import LTS
 
 -- Generate all the Straces of the given IOLTS
 straces :: IOLTS -> [Trace]
-straces (states, inputs, outputs, transitions, initial) = undefined
+straces (states, inputs, outputs, transitions, initial) = do
+    tracesFromState (states, inputs, outputs, transitions, initial) initial
+
+-- Generate all the traces from the given state
+tracesFromState :: IOLTS -> State -> [Trace]
+tracesFromState (states, inputs, outputs, transitions, initial) state = do
+    let transitionsFromState = filter (\(from, _, _) -> from == state) transitions
+    if null transitionsFromState
+        then [[]]
+        else do
+            (from, label, to) <- transitionsFromState
+            traces <- tracesFromState (states, inputs, outputs, transitions, initial) to
+            return (label : traces)
