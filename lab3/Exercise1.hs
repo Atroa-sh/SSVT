77da2a42a3e0a861e99816fdc67b0debbd05f272
Devrim Yugnuk <devrim.yugnuk@student.uva.nl>
2023-10-04 15:36:00 +0200

    Fixed ex1

--- lab3/Exercise1.hs
+++ lab3/Exercise1.hs
@@ -1,5 +1,7 @@
 module Exercise1 where
 
+import Test.QuickCheck
+
 {-
  Types of outputs which are not yet covered by mutations in mutation.hs
  1. Shuffle the order of the list
@@ -10,3 +12,23 @@ module Exercise1 where
  6. Remove elements from a random positions in the list
  7. Perform arithmic operations on any or all elements in the list (add, sub, mul, div, mod, neg, reverse, etc.)
 -}
+
+-- 3:
+removeAllElements :: [Integer] -> Gen [Integer]
+removeAllElements xs = return []
+
+-- 4:
+removeElementsFromStart :: [Integer] -> Gen [Integer]
+removeElementsFromStart xs = choose (1, length xs - 1) >>= \x -> return $ drop x xs
+
+-- 5:
+removeElementsFromMiddle :: [Integer] -> Gen [Integer]
+removeElementsFromMiddle xs = choose (1, length xs - 1) >>= \x -> return $ take x xs ++ drop (x + 1) xs
+
+-- 6:
+removeElementsFromRandomPosition :: [Integer] -> Gen [Integer]
+removeElementsFromRandomPosition xs = choose (1, length xs - 1) >>= \x -> return $ take (x - 1) xs ++ drop x xs
+
+-- 7:
+performArithmicOperations :: [Integer] -> Gen [Integer]
+performArithmicOperations xs = return $ map negate xs
