f9687201c6a482a3ce5afb8d22f1ac0726c58163
Devrim Yugnuk <devrim.yugnuk@student.uva.nl>
2023-10-04 15:25:40 +0200

    Ex1

--- lab3/Exercise1.hs
+++ lab3/Exercise1.hs
@@ -1,1 +1,12 @@
 module Exercise1 where
+
+{-
+ Types of outputs which are not yet covered by mutations in mutation.hs
+ 1. Shuffle the order of the list
+ 2. Add elements to random positions in the list
+ 3. Remove all elements from the list (empty list)
+ 4. Remove elements from the start of the list
+ 5. Remove elements from the middle of the list
+ 6. Remove elements from a random positions in the list
+ 7. Perform arithmic operations on any or all elements in the list (add, sub, mul, div, mod, neg, reverse, etc.)
+-}
