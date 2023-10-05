7a5891c0c122f07696df2d92c90c39133478433b
Devrim Yugnuk <devrim.yugnuk@student.uva.nl>
2023-10-05 14:28:23 +0200

    Added new files

---
+++ lab3/Exercise3.hs
@@ -0,0 +1,9 @@
+module Exercise3 where
+
+import Data.List
+import Test.QuickCheck
+import Mutation
+import MultiplicationTable
+
+-- Implement a function that calculates the minimal property subsets given a function under test and a set of properties
+minimalPropertySubsets :: (Integer -> [Integer]) -> [[Integer] -> Integer -> Bool] -> ...
